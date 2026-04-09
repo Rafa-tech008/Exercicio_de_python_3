56b7e1f512f0ec6f2b20ad6e7a97696ec4750b5a
Rafa_Ramos <rafaelramos7acsc@gmail.com>
2026-04-09 17:52:41 -0300

    Update exercicio20.py

--- exercicio20.py
+++ exercicio20.py
@@ -1,39 +1,32 @@
-#EST.MOD_LT01.19
+#EST.MOD_LT01.20
 #Declaração de variáveis
-a=0
-b=0
-c=0
+n1=0.0
+n2=0.0
+n3=0.0
+n4=0.0
 
 #Início
-def calc_2grau():
-    global a , b , c
-    delta=((b**2)-(4*a*c))
-    if (delta<0):
-        print ("Não há nenhuma raíz e o delta é negativo")
+def calc_notas():
+    global n1, n2, n3, n4
+    media=(n1+n2+n3+n4)/4
+    if (media>10):
+        print("Inválido")
         return
-    elif (delta==0):
-        print ("Há apenas uma raíz e delta é:",delta)
+    elif (media>=6.0):
+        print("Aprovado, sua média foi:",media)
+    elif (media<6.0 and media>=3.0):
+        print("Exame, sua média foi:",media)
     else:
-        print ("Há duas raízes e delta é:",delta)
-        
-    raiz1=(-(-b)+(delta**0.5)/2*a)
-    raiz2=(-(-b)-(delta**0.5)/2*a)
-    if (raiz1==raiz2):
-        print ("O valor da raiz é:",raiz1)
-    elif (raiz1>0 and raiz2>0):
-        print (f"O valor das raízes são: {raiz1} e {raiz2}.")
-    elif (raiz1<0 and raiz2<0):
-        print ("O valor da raíz é:",raiz2)
-    else:
-        print ("O valor da raíz é:",raiz1)
+        print("Reprovado, sua média foi:",media)
         
 def main():
-    global a , b , c
-    print("--- Exercicio 20 ---")
-    a=int(input("Insira o primeiro valor: "))
-    b=int(input("Insira o segundo valor: "))
-    c=int(input("Insira o terceiro valor: "))
-    calc_2grau()
+    global n1, n2, n3, n4
+    print("--- Exercicio 21 ---")
+    n1=float(input("Insira a primeira nota: "))
+    n2=float(input("Insira a segunda nota: "))
+    n3=float(input("Insira a terceira nota: "))
+    n4=float(input("Insira a última nota: "))
+    calc_notas()
     
 if (__name__ == "__main__"):
     main()
